240280beaffd30dd3527b9da00358081e5c61807
Sasanka742 <league@WTS20.attlocal.net>
2019-05-05 13:28:00 -0700

    Added basic api functionality

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const app = express()
+
+app.use(bodyparser.json())
+bodyparser.urlencoded({
+	extended: true
+}
+)
+app.get('/getQuiz', (req, res) => {
+    res.send([])
+})
+app.post('/getQuiz', (req,res) => {
+    res.send(req.body)
+})
+
+app.listen(8080, () => console.log("Server Started on Port 8080!!!"))
