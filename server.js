ade6bee16bf6319c3a9dc4c62997a7cbc6e94d63
League <league@iMac-3.local>
2019-08-11 13:32:11 -0700

    basic backend with mock data

--- server.js
+++ server.js
@@ -8,9 +8,43 @@ bodyparser.urlencoded({
 }
 )
 app.get('/getQuiz', (req, res) => {
-    res.send([])
+    res.send({
+        "headers": [
+          "Quiz Name",
+          "Author",
+          "Create Date",
+          "Action"
+        ],
+        "rows": [
+          [
+            "Quiz 1",
+            "John Smith",
+            "10/25/18",
+            "ACTION"
+          ],
+          [
+            "Quiz 2",
+            "John Smith",
+            "10/32/18",
+            "ACTION"
+          ],
+          [
+            "Quiz 3",
+            "John Smith",
+            "11/7/18",
+            "ACTION"
+          ],
+          [
+            "Quiz 4",
+            "John Smith",
+            "11/7/18",
+            "ACTION"
+          ]
+        ]
+      });
 })
 app.post('/getQuiz', (req,res) => {
+    console.log(req.body);
     res.send(req.body)
 })
 
